e68641cfc0280ca3ac49943d66dbc1d324947860
wcalvert88 <wcalvert88@gmail.com>
2018-08-27 14:14:28 -0400

    initial commit

---
+++ app/sql/local.wp_usermeta.sql
@@ -0,0 +1,24 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_usermeta` VALUES
+(1,1,"nickname","wcalvert88"),
+(2,1,"first_name",""),
+(3,1,"last_name",""),
+(4,1,"description",""),
+(5,1,"rich_editing","true"),
+(6,1,"syntax_highlighting","true"),
+(7,1,"comment_shortcuts","false"),
+(8,1,"admin_color","fresh"),
+(9,1,"use_ssl","0"),
+(10,1,"show_admin_bar_front","true"),
+(11,1,"locale",""),
+(12,1,"wp_capabilities","a:1:{s:13:\"administrator\";b:1;}"),
+(13,1,"wp_user_level","10"),
+(14,1,"dismissed_wp_pointers","wp496_privacy"),
+(15,1,"show_welcome_panel","1"),
+(16,1,"session_tokens","a:1:{s:64:\"1c55a2b328bba46c6cddc962a3a2555287291daebe7f5540d7e5884328cb13a1\";a:4:{s:10:\"expiration\";i:1535830542;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:114:\"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36\";s:5:\"login\";i:1534620942;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","4"),
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
+(20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}");
---
+++ app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"wcalvert88","$P$BROx7bd/bxuiAR6YaeJfepH8MeIbP9/","wcalvert88","wcalvert88@gmail.com","","2018-08-18 19:30:33","",0,"wcalvert88");
